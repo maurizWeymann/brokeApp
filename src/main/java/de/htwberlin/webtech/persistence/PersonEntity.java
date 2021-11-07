e66a6e1036162462eacbb7551c630ebe92db05eb
maurizWeymann <s0528279@htw-berlin.de>
2021-11-07 02:10:50 +0100

    add changes and implementations to entity manipulatedRequest Repository restController service

---
+++ src/main/java/de/htwberlin/webtech/persistence/PersonEntity.java
@@ -0,0 +1,57 @@
+package de.htwberlin.webtech.persistence;
+
+import javax.persistence.*;
+
+@Entity(name = "persons")
+public class PersonEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "is_vaccinated")
+    private boolean vaccinated;
+
+    public PersonEntity(String firstName, String lastName, boolean vaccinated) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.vaccinated = vaccinated;
+    }
+
+    protected PersonEntity() {    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean getVaccinated() {
+        return vaccinated;
+    }
+
+    public void setVaccinated(boolean vaccinated) {
+        this.vaccinated = vaccinated;
+    }
+}
